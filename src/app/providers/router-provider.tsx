64e4796f0071875617cdfe368239ccf3ade4693d
dskorik <dskorik@team.amocrm.com>
2026-01-02 09:12:47 +0300

    refactor: restructure routing setup using routesMap for better readability and maintainability

--- src/app/providers/router-provider.tsx
+++ src/app/providers/router-provider.tsx
@@ -4,32 +4,47 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from 'react-router-dom';
-import { HomePage } from '../../pages/home';
-import { DifficultyPage } from '../../pages/difficulty';
-import { GamePage } from '../../pages/game';
-import { CategoryPage } from '../../pages/category';
-import { ResultPage } from '../../pages/result';
+import { HomePage } from '@/pages/home';
+import { DifficultyPage } from '@/pages/difficulty';
+import { GamePage } from '@/pages/game';
+import { CategoryPage } from '@/pages/category';
+import { ResultPage } from '@/pages/result';
 
-const routeKeys = {
-  home: '/',
-  category: 'category/',
-  difficulty: 'category/:categoryName/',
-  match: 'category/:categoryName/difficulty/:difficultyLevel/',
-  result: 'category/:categoryName/difficulty/:difficultyLevel/game-result/',
+const routesMap = {
+  home: {
+    path: '/',
+    component: <HomePage />,
+  },
+  category: {
+    path: 'category/',
+    component: <CategoryPage />,
+  },
+  difficulty: {
+    path: 'category/:categoryName/',
+    component: <DifficultyPage />,
+  },
+  match: {
+    path: 'category/:categoryName/difficulty/:difficultyLevel/',
+    component: <GamePage />,
+  },
+  result: {
+    path: 'category/:categoryName/difficulty/:difficultyLevel/game-result/',
+    component: <ResultPage />,
+  },
+};
+
+const createRoutes = () => {
+  return Object.entries(routesMap).map(([key, value]) => {
+    const { component, path } = value;
+
+    return <Route key={key} path={path} element={component} />;
+  });
 };
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path={routeKeys.home} element={<HomePage />} />
-      <Route path={routeKeys.category} element={<CategoryPage />} />
-      <Route path={routeKeys.difficulty} element={<DifficultyPage />} />
-      <Route path={routeKeys.match} element={<GamePage />} />
-      <Route path={routeKeys.result} element={<ResultPage />} />
-    </>
-  )
+  createRoutesFromElements(<>{createRoutes()}</>)
 );
 
-export function BrowserRouter() {
+export const BrowserRouter = () => {
   return <RouterProvider router={router} />;
-}
+};
