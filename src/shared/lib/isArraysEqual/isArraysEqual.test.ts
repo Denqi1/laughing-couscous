329b643766fe1fffbb0cb6f6a11ac3e7d191e1fa
dskorik <dskorik@team.amocrm.com>
2026-03-12 12:48:48 +0300

    feat: added describe

--- src/shared/lib/isArraysEqual/isArraysEqual.test.ts
+++ src/shared/lib/isArraysEqual/isArraysEqual.test.ts
@@ -1,30 +1,32 @@
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { isArraysEqual } from './isArraysEqual';
 
-test('returns true for two empty arrays', () => {
-  expect(isArraysEqual([], [])).toBe(true);
-});
+describe('isArraysEqual', () => {
+  test('returns true for two empty arrays', () => {
+    expect(isArraysEqual([], [])).toBe(true);
+  });
 
-test('returns true for arrays with identical strings', () => {
-  expect(isArraysEqual(['a'], ['a'])).toBe(true);
-});
+  test('returns true for arrays with identical strings', () => {
+    expect(isArraysEqual(['a'], ['a'])).toBe(true);
+  });
 
-test('returns true for two arrays with different order but identical values', () => {
-  expect(isArraysEqual(['a', 'b'], ['b', 'a'])).toBe(true);
-});
+  test('returns true for two arrays with different order but identical values', () => {
+    expect(isArraysEqual(['a', 'b'], ['b', 'a'])).toBe(true);
+  });
 
-test('returns false for two arrays with different strings', () => {
-  expect(isArraysEqual(['a'], ['b'])).toBe(false);
-});
+  test('returns false for two arrays with different strings', () => {
+    expect(isArraysEqual(['a'], ['b'])).toBe(false);
+  });
 
-test('returns true when duplicates count matches', () => {
-  expect(isArraysEqual(['a', 'a'], ['a', 'a'])).toBe(true);
-});
+  test('returns true when duplicates count matches', () => {
+    expect(isArraysEqual(['a', 'a'], ['a', 'a'])).toBe(true);
+  });
 
-test('returns false when duplicates count differs', () => {
-  expect(isArraysEqual(['a', 'a'], ['a', 'b'])).toBe(false);
-});
+  test('returns false when duplicates count differs', () => {
+    expect(isArraysEqual(['a', 'a'], ['a', 'b'])).toBe(false);
+  });
 
-test('returns false if the duplicates are different.', () => {
-  expect(isArraysEqual(['a', 'a', 'b'], ['a', 'b', 'b'])).toBe(false);
+  test('returns false if the duplicates are different.', () => {
+    expect(isArraysEqual(['a', 'a', 'b'], ['a', 'b', 'b'])).toBe(false);
+  });
 });
